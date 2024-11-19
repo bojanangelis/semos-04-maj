f2ff806c3997b9c7a2ec6296aa7b0f1f82a66e75
bojanangelis <bojanangeleski98@gmail.com>
2024-11-19 22:00:52 +0100

    lecture 01

---
+++ lecture_01/vite-project/src/LectureApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LectureApp = () => {
+    const name = 'Bojan Angjeleski'
+    const age = 26
+  return (
+    <div>
+        <h2>{name}</h2>
+        <h2>{age}</h2>
+    </div>
+  )
+}
+
+export default LectureApp
